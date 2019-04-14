915c291a509e217153f7f665cb0416a8b13b960b
Adam Christopher <adam.keristoper@gmail.com>
2019-04-15 00:39:33 +0900

    Add html webpack plugin
    dev & production working

--- web/recipe/webpack.config.js
+++ web/recipe/webpack.config.js
@@ -1,11 +1,11 @@
 const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
 
 const config = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'app.min.js',
-    publicPath: '/dist/'
+    filename: 'app.min.js'
   },
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
@@ -17,6 +17,11 @@ const config = {
       { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
     ]
   },
+  plugins: [
+    new htmlWebpackPlugin({
+      title: 'MumuLala Recipe'
+    })
+  ],
   mode: 'development'
 }
 
