75222116b9a90348be08de8515385637eea4b02d
Adam Christopher <adam.keristoper@gmail.com>
2019-04-15 00:50:29 +0900

    Add babel env with html template

--- web/recipe/webpack.config.js
+++ web/recipe/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
-const htmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 const config = {
   entry: './src/index.js',
@@ -14,15 +15,29 @@ const config = {
   },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      { test: /\.vue$/, exclude: /node_modules/, loader: "vue-loader" }
     ]
   },
   plugins: [
-    new htmlWebpackPlugin({
-      title: 'MumuLala Recipe'
-    })
+    new HtmlWebpackPlugin({
+      title: 'MumuLala Recipe',
+      template: 'src/index.html'
+    }),
+    new VueLoaderPlugin()
   ],
   mode: 'development'
+}
+
+if(process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  )
 }
 
 module.exports = config
