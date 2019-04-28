9b8b1091fead123eeb9ae9c9b487c04f94a73832
Adam Christopher <adam.keristoper@gmail.com>
2019-04-28 11:20:01 +0900

    Stub router

--- web/recipe/test/components/layout/Navigation.test.js
+++ web/recipe/test/components/layout/Navigation.test.js
@@ -1,14 +1,18 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils'
 import Navigation from 'components/layout/Navigation'
+import VueRouter from 'vue-router'
 
 const localVue = createLocalVue()
+localVue.use(VueRouter)
+const router = new VueRouter()
 
 describe('Navigation', () => {
   let wrapper
 
   beforeEach(() => {
     wrapper = shallowMount(Navigation, {
-      localVue
+      localVue,
+      router
     })
   })
 
