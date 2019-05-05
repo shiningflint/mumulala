55e0ed6b500ab6de4221422423d62be3d70b30f7
Adam Christopher <adam.keristoper@gmail.com>
2019-05-05 19:35:39 +0900

    Simple irb console

--- api/models.rb
+++ api/models.rb
@@ -1,4 +1,9 @@
 require_relative 'db'
 require 'sequel/model'
 
+unless defined?(Unreloader)
+  require 'rack/unreloader'
+  Unreloader = Rack::Unreloader.new(reload: false)
+end
+
 Unreloader.require('models'){ |f| Sequel::Model.send(:camelize, File.basename(f).sub(/\.rb\z/, '')) }
