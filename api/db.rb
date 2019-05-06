660c8eaf671396a9a62009e31444c2009ddaa512
Adam Christopher <adam.keristoper@gmail.com>
2019-05-06 10:04:26 +0900

    use rack_env for develop and test and more on rakefile

--- api/db.rb
+++ api/db.rb
@@ -7,8 +7,8 @@ DB_ENV = Dotenv.load
 
 DB = Sequel.connect(
   adapter: 'postgres',
-  host: DB_ENV.delete('DB_HOST'),
-  database: DB_ENV.delete('DB'),
-  user: DB_ENV.delete('DB_USER'),
-  password: DB_ENV.delete('DB_PASSWORD')
+  host: DB_ENV.delete("DB_HOST_#{ENV['RACK_ENV'].upcase}"),
+  database: DB_ENV.delete("DB_#{ENV['RACK_ENV'].upcase}"),
+  user: DB_ENV.delete("DB_USER_#{ENV['RACK_ENV'].upcase}"),
+  password: DB_ENV.delete("DB_PASSWORD_#{ENV['RACK_ENV'].upcase}")
 )
