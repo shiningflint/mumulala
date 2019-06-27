5928767677d1a5133148fca3afc01ef92334a27c
Adam Christopher <adam.keristoper@gmail.com>
2019-06-27 22:02:34 +0900

    Add comments and puts

--- api/db.rb
+++ api/db.rb
@@ -5,6 +5,8 @@ abort 'No env files found in db.rb' if Dotenv.load.empty?
 
 DB_ENV = Dotenv.load
 
+# Delete APP_DATABASE_URL from the environment, so it isn't accidently
+# passed to subprocesses.
 DB = Sequel.connect(
   adapter: 'postgres',
   host: DB_ENV.delete("DB_HOST_#{ENV['RACK_ENV'].upcase}"),
