10f93e1939fb944decdc53b431699bd46c2f7713
Adam Christopher <adam.keristoper@gmail.com>
2019-07-23 21:27:48 +0900

    Create image uploader

---
+++ web/recipe/src/components/ImageUploader.vue
@@ -0,0 +1,22 @@
+<template>
+  <input
+    accept="image/*"
+    capture="camera"
+    type="file"
+    :value="value"
+    @input="$emit('input', $event.target.value)"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- web/recipe/src/views/Recipes/Edit.vue
+++ web/recipe/src/views/Recipes/Edit.vue
@@ -2,19 +2,36 @@
   <div class="full-height">
     <layout-header title="レシピー"/>
     <page-content>
-      <p>kentang panas</p>
+      <form @submit.prevent="onSubmit">
+        <image-uploader v-model="form.image" />
+        <button type="submit">Save</button>
+      </form>
     </page-content>
   </div>
 </template>
 
 <script>
+import ImageUploader from 'components/ImageUploader'
 import LayoutHeader from 'components/layout/Header'
 import PageContent from 'components/layout/PageContent'
 
 export default {
   components: {
+    ImageUploader,
     LayoutHeader,
     PageContent,
+  },
+  data () {
+    return {
+      form: {
+        image: null,
+      },
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log('submitting form', { ...this.form, })
+    },
   },
 }
 </script>
